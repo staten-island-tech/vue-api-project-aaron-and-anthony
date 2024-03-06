30d3c15cfb577d3f91ea7d6fc6f9ffab94c2e95c
Aaron-Ku06 <aaronku320@gmail.com>
2024-03-06 09:21:56 -0500

    views

--- vite-project/src/views/HomeView.vue
+++ vite-project/src/views/HomeView.vue
@@ -1,9 +1,10 @@
 <script setup>
+import HomeGraph from '@/components/HomeGraph.vue';
 import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <HomeGraph />
   </main>
 </template>
