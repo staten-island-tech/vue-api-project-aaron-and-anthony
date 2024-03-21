596b66a1aa8f9bdd32e8e9bc9cb67c0cfee0e598
Aaron-Ku06 <aaronku320@gmail.com>
2024-03-21 09:22:04 -0400

    dont work

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -1,4 +1,4 @@
-<!-- <script setup>
+<script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
 </script>
@@ -82,39 +82,7 @@ nav a:first-of-type {
     margin-top: 1rem;
   }
 }
-</style> -->
+</style>
 
 
-<template>
-  <div class="container">
-    <Bar v-if="loaded" :data="chartData" />
-  </div>
-</template>
 
-<script>
-import { Bar } from 'vue-chartjs'
-import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
-
-ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
-
-export default {
-  name: 'BarChart',
-  components: { Bar },
-  data: () => ({
-    loaded: false,
-    chartData: null
-  }),
-  async mounted () {
-    this.loaded = false
-
-    try {
-      const { userlist } = await fetch('https://data.cityofnewyork.us/resource/ykvb-493p.json')
-      this.chartdata = userlist
-
-      this.loaded = true
-    } catch (e) {
-      console.error(e)
-    }
-  }
-}
-</script>
