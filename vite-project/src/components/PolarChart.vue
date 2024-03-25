7dc0cdf961e8476cd02bbc3dce67d82b2b700981
AnthonyPvZ <anthonypvz123@gmail.com>
2024-03-25 09:10:59 -0400

    cool

---
+++ vite-project/src/components/PolarChart.vue
@@ -0,0 +1,80 @@
+<template>
+    <div class="container">
+      <PolarArea v-if="loaded" :data="chartData" />
+    </div>
+  </template>
+  
+  <script>
+  import {
+  Chart as ChartJS,
+  RadialLinearScale,
+  ArcElement,
+  Tooltip,
+  Legend
+} from 'chart.js'
+import { PolarArea } from 'vue-chartjs'
+
+ChartJS.register(RadialLinearScale, ArcElement, Tooltip, Legend)
+
+
+  export default {
+    name: 'PolarChart',
+    components: { PolarArea },
+    data: () => ({
+      loaded: false,
+      chartData: null
+    }),
+    async mounted () {
+      this.loaded = false
+  
+      try {
+        const res = await fetch('https://data.cityofnewyork.us/resource/2rb7-7eqa.json')
+        const crimes = await res.json()
+  
+        let sort_by_races = {}
+        let unique_races = []
+        let crime_count_by_race = []
+  
+        crimes.forEach((crime)=>{
+          let race = crime.suspect_race
+  
+          if (race == undefined) race = "REDACTED"
+  
+          if (!sort_by_races.hasOwnProperty(race)){
+            sort_by_races[race] = [crime]
+            unique_races.push(race)
+          }else{
+            sort_by_races[race].push(crime)
+          }
+  
+        })
+        
+        for (let race of unique_races){
+          crime_count_by_race.push(sort_by_races[race].length)
+        }
+  
+        this.chartData = {
+          labels: unique_races,
+          datasets: [
+            {
+              label: "Black people doing black things",
+              backgroundColor: '#f87979',
+              data: crime_count_by_race
+            }
+          ]
+        }
+  
+        this.loaded = true
+      } catch (e) {
+        console.error(e)
+      }
+    }
+  }
+  </script>
+  
+  <style scoped>
+  .container{
+    height: 90vh;
+    width: 70vw;
+  }
+  </style>
--- vite-project/src/views/HomeView.vue
+++ vite-project/src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
 import BarChart from '../components/BarChart.vue'
+import PolarChart from '../components/PolarChart.vue'
 </script>
 
 <template>
   <main>
     <BarChart />
+    <PolarChart />
   </main>
 </template>
